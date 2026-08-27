0473d71e2853497406d0fe3c61144df6b6d09d8b
ronit joshi <ronit557joshi@gmail.com>
2026-08-27 16:22:05 +0530

    Day--27/08

---
+++ Template Literals.js
@@ -0,0 +1,15 @@
+//alert("why are you here, at this time?")
+
+// Use of Template literals (`${}`) in JavaScript
+let num1 = 4
+let num2 = 6
+let result = num1+num2
+
+ //insert of doing --console.log(" The sum of " +num1 + " and " +num2 + " is " +result)
+console.log(`the sum of ${num1} and ${num2} is ${result}`)
+
+
+
+ //second use of Template Literals
+console.log(`we are changing the line without using --/n-- method insert of using
+    This  method`)
